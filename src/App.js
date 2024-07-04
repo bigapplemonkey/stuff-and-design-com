8cb93b94838422abef54dc7953591321b63192b8
Jorge Asuaje <jorge.asuaje@gmail.com>
2024-07-04 01:13:39 -0400

    Implement App loading animation with overlay

--- src/App.js
+++ src/App.js
@@ -1,34 +1,44 @@
 import { Routes, Route, useLocation } from 'react-router-dom';
 import Home from './layouts/Home';
 import Navigation from './components/Navigation';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { DataContext } from './context/DataContext';
 import { motion } from 'framer-motion';
+import AppLoader from './components/animations/AppLoader';
+import { ANIMATION } from './config';
 
 function App() {
   const location = useLocation();
   const key = location.pathname;
   const { loading } = useContext(DataContext);
+  const [loadingComplete, setLoadingComplete] = useState(false);
+
+  const { CUBIC_BEZIER } = ANIMATION;
 
   if (loading) {
     return null;
   }
 
   return (
-    <motion.div
-      className="container full-height"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ delay: 0.3 }}
-    >
-      <Navigation />
-      <Routes location={location} key={key}>
-        <Route index element={<Home />} />
-        <Route path="/work" element={<h1>Work</h1>} />
-        <Route path="/about" element={<h1>About</h1>} />
-        <Route path="/contact" element={<h1>Contact</h1>} />
-      </Routes>
-    </motion.div>
+    <>
+      {!loadingComplete && (
+        <AppLoader setLoadingComplete={setLoadingComplete} />
+      )}
+      <motion.div
+        className="container full-height"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: loadingComplete ? 1 : 0 }}
+        transition={{ duration: 0.5, delay: 0.1, ease: CUBIC_BEZIER }}
+      >
+        <Navigation />
+        <Routes location={location} key={key}>
+          <Route index element={<Home />} />
+          <Route path="/work" element={<h1>Work</h1>} />
+          <Route path="/about" element={<h1>About</h1>} />
+          <Route path="/contact" element={<h1>Contact</h1>} />
+        </Routes>
+      </motion.div>
+    </>
   );
 }
 
---
+++ src/components/animations/AppLoader.jsx
@@ -0,0 +1,79 @@
+import { motion, useAnimation } from 'framer-motion';
+import { useEffect, useState } from 'react';
+import { ANIMATION } from '../../config';
+
+const AppLoader = ({ setLoadingComplete }) => {
+  const [progress, setProgress] = useState(0);
+  const windowControls = useAnimation();
+  const containerControls = useAnimation();
+  const overlayControls = useAnimation();
+
+  const { CUBIC_BEZIER } = ANIMATION;
+
+  useEffect(() => {
+    const animateLoading = async () => {
+      await playAnimations(containerControls, windowControls, overlayControls);
+      setLoadingComplete(true);
+    };
+
+    const interval = setInterval(() => {
+      setProgress(prevProgress => {
+        const nextProgress = prevProgress >= 100 ? 100 : prevProgress + 1;
+        if (nextProgress === 100) {
+          clearInterval(interval);
+          animateLoading();
+        }
+        return nextProgress;
+      });
+    }, 37);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const playAnimations = async (container, window, overlay) => {
+    await container.start({
+      opacity: 0,
+      transition: { duration: 0.3, delay: 0.2 },
+    });
+    await window.start({
+      width: '100vw',
+      height: '100vh',
+      transition: { duration: 0.3, ease: CUBIC_BEZIER },
+    });
+    await overlay.start({ opacity: 0, transition: { duration: 0.1 } });
+    await window.start({
+      opacity: 0,
+      transition: { duration: 0.1, ease: CUBIC_BEZIER },
+    });
+  };
+
+  return (
+    <motion.div className="loading-overlay" animate={overlayControls}>
+      <motion.div className="loading-window" animate={windowControls}>
+        <motion.div className="loading-content" animate={containerControls}>
+          <div className="loading-title-bar">
+            <div className="loading-title loading-type">
+              Loading...<span className="loading-pulsing">.</span>
+              Stuff_and_Design
+              <span className="loading-welcome"> (Welcome)</span>
+            </div>
+          </div>
+          <div className="loading-container">
+            <div className="loading-bar">
+              <motion.div
+                className="loading-progress"
+                style={{ width: `${progress}%` }}
+                initial={{ width: '0%' }}
+                animate={{ width: `${progress}%` }}
+                transition={{ duration: 0.2 }}
+              />
+            </div>
+            <div className="loading-counter loading-type">{`${progress}%`}</div>
+          </div>
+        </motion.div>
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default AppLoader;
