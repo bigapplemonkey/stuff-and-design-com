b951ec6caf805ff61c75908007f10c223357d53c
Jorge Asuaje <jorge.asuaje@gmail.com>
2024-09-08 14:01:40 -0400

    Add ScrollToTop component to ensure page scroll resets on page transitions

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { DataContextProvider } from './context/DataContext';
+import ScrollToTop from './components/ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <DataContextProvider>
       <Router>
+        <ScrollToTop delayInSeconds={0.95} />
         <App />
       </Router>
     </DataContextProvider>
